94a9ca93ae4f540c10a4839bd025c5b7b4ee2ec7
parksol <thfdl0317@naver.com>
2024-08-05 03:28:51 +0900

    feat : styled-components 적용

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+import styled from "styled-components/native";
 
-export default function App() {
+export type Props = {
+  label: string;
+  color?: string;
+  onPress?: (data?: any) => void;
+} & TouchableOpacityProps;
+
+const App: React.FC<Props> = ({ label, onPress, color, ...rest }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>스타일 컴포넌트 적용</Text>
       <StatusBar style="auto" />
+      <StyledButton onPress={onPress} color={color} {...rest}>
+        <StyledText>{label}</StyledText>
+      </StyledButton>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+const StyledButton = styled(TouchableOpacity)<{ color?: string }>`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  background-color: ${({ color }) => (color ? color : "green")};
+`;
+
+const StyledText = styled.Text`
+  color: white;
+`;
+
+export default App;
