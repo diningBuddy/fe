050c75966956186e3cc5ddb031de3b39bd1b119b
Bob Sin <tkagmd1@naver.com>
2024-09-17 02:52:41 +0900

    [KAN-187] 프로젝트 초기 스타일 설정 - 인풋 (#3)
    * [KAN-187] 프로젝트 초기 스타일 설정 - 인풋
    
    * [KAN-187] 프로젝트 초기 스타일 설정 - 인풋
    
    * [KAN-187] props 설정 추가
    
    * [KAN-187] default 값 추가
    
    * [KAN-187] label, description margin 추가
    
    * [KAN-187] theme 연결
    
    * [KAN-187] 앱 설정, input button 색처리
    
    * [KAN-187] label 작업
    
    * [KAN-187] input wrapper
    
    * [KAN-187] styled input
    
    * styleInput 완료
    
    * descriptionText 완료
    
    * close button
    
    * close button 색상
    
    * lint
    
    * 초기인풋값 변경
    
    * 필요없는값 제거
    
    * width 제거
    
    * inputbutton -> input
    
    * input 위치 변경
    
    * [KAN-187] 테두리 추가
    
    * [KAN-187] 테두리 값 변경
    
    * [KAN-187] margin
    
    * [KAN-187] 타입 명시 추가
    
    * [KAN-197] 색상 재설정 컴파일 오류 제거
    
    * [KAN-187] compile error 추가 수정
    
    * [KAN-187] circle close svg 변환
    
    * [KAN-187] 바깥색 변경
    
    * [KAN-187] 배경색 변경, border 변경
    
    ---------
    
    Co-authored-by: parksol <thfdl0317@naver.com>

--- App.tsx
+++ App.tsx
@@ -1,30 +1,21 @@
-import React, { useContext } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React, {useContext} from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from "react-native";
-import styled, {
-  ThemeProvider,
-  ThemeContext,
-  DefaultTheme,
-} from "styled-components/native";
+import {TouchableOpacityProps,} from "react-native";
+import {ThemeContext, ThemeProvider,} from "styled-components/native";
 import ThemeStyle from "./src/styles/ThemeStyle";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ListScreen from "./src/screens/ListScreen";
 import MapScreen from "./src/screens/MapScreen";
-import { RootStackParamList, RouteNames } from "./src/utils/routes";
+import {RootStackParamList, RouteNames} from "./src/utils/routes";
 import MypageScreen from "./src/screens/MypageScreen";
 import BrowserScreen from "./src/screens/BrowserScreen";
 import SampleColor from "./src/components/sample/SampleColor";
 import SampleFont from "./src/components/sample/SampleFont";
 import SampleButton from "./src/components/sample/SampleButton";
+import SampleInput from "./src/components/sample/SampleInput";
 
 export type Props = {
   label: string;
@@ -37,40 +28,41 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const HomeTab = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={RouteNames.HOME} component={HomeScreen} />
-      <Tab.Screen name={RouteNames.LIST} component={ListScreen} />
-      <Tab.Screen name={RouteNames.MAP} component={MapScreen} />
-      <Tab.Screen name={RouteNames.MYPAGE} component={MypageScreen} />
-    </Tab.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name={RouteNames.HOME} component={HomeScreen}/>
+        <Tab.Screen name={RouteNames.LIST} component={ListScreen}/>
+        <Tab.Screen name={RouteNames.MAP} component={MapScreen}/>
+        <Tab.Screen name={RouteNames.MYPAGE} component={MypageScreen}/>
+      </Tab.Navigator>
   );
 };
 
-const App: React.FC<Props> = ({ label, onPress, color, ...rest }) => {
+const App: React.FC<Props> = ({label, onPress, color, ...rest}) => {
   const theme = useContext(ThemeContext) || ThemeStyle;
 
   return (
-    <ThemeProvider theme={ThemeStyle}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name={RouteNames.HOME_TAB}
-            component={HomeTab}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name={RouteNames.BROWSER} component={BrowserScreen} />
-          <Stack.Screen
-            name={RouteNames.SAMPLE_COLOR}
-            component={SampleColor}
-          />
-          <Stack.Screen name={RouteNames.SAMPLE_FONT} component={SampleFont} />
-          <Stack.Screen
-            name={RouteNames.SAMPLE_BUTTON}
-            component={SampleButton}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ThemeProvider>
+      <ThemeProvider theme={ThemeStyle}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+                name={RouteNames.HOME_TAB}
+                component={HomeTab}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen name={RouteNames.BROWSER} component={BrowserScreen}/>
+            <Stack.Screen
+                name={RouteNames.SAMPLE_COLOR}
+                component={SampleColor}
+            />
+            <Stack.Screen name={RouteNames.SAMPLE_FONT} component={SampleFont}/>
+            <Stack.Screen
+                name={RouteNames.SAMPLE_BUTTON}
+                component={SampleButton}
+            />
+            <Stack.Screen name={RouteNames.SAMPLE_INPUT} component={SampleInput}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
   );
 };
 
---
+++ src/common/Input.tsx
@@ -0,0 +1,154 @@
+import React, {useContext, useEffect, useState} from "react";
+import styled, {DefaultTheme, ThemeContext} from "styled-components/native";
+import {TextInput, TouchableOpacity} from "react-native";
+import {BodyMedium14, BodyRegular12} from "./Typo";
+import {CircleClose} from "../assets/icons/shape";
+
+interface InputProps {
+  variant?: "default" | "destructive";
+  state?: "initial" | "focused" | "disabled";
+  label?: string;
+  description?: string;
+  inputValue?: string;
+  placeholder?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+                                       variant = "default",
+                                       state = "initial",
+                                       label = "",
+                                       description = "",
+                                       inputValue = "",
+                                       placeholder = "Type your message here",
+                                     }) => {
+  const theme = useContext(ThemeContext);
+
+  // State to manage the value of the input fields
+  const [value, setValue] = useState(inputValue);
+
+  useEffect(() => {
+    setValue(inputValue);
+  }, [inputValue]);
+
+  // Function to clear the input field
+  const clearInput = () => {
+    setValue("");
+  };
+
+  return (
+      <InputRow>
+        {label && <Label theme={theme}>{label}</Label>}
+        <InputWrapper variant={variant} state={state} theme={theme}>
+          <InputContainer variant={variant} state={state} theme={theme}>
+            <StyledInput
+                theme={theme}
+                placeholder={placeholder}
+                value={value}
+                onChangeText={setValue}
+                editable={state !== "disabled"}
+            />
+
+            <CloseButton onPress={clearInput} disabled={state === "disabled"}>
+              <CircleClose/>
+            </CloseButton>
+          </InputContainer>
+        </InputWrapper>
+        {description && (
+            <DescriptionText variant={variant} theme={theme}>
+              {description}
+            </DescriptionText>
+        )}
+      </InputRow>
+  );
+};
+
+const InputRow = styled.View`
+  margin-bottom: 24px; /* Increased margin to accommodate the description text */
+`;
+
+const Label = styled(BodyMedium14).attrs(({theme}: { theme: DefaultTheme }) => ({
+  color: theme.color.global.neutral[800],
+}))`
+  margin: 6px 0 0 3px;
+`;
+
+const InputWrapper = styled.View<InputProps & { theme: DefaultTheme }>`
+  border: 1px solid ${({variant, state, theme}) => {
+    if (variant === "default") {
+      return state === "focused"
+          ? "transparent" :
+          theme.color.theme.border;
+    } else if (variant === "destructive") {
+      return state === "focused"
+          ? "transparent"
+          : theme.color.sys.destructive.default;
+    }
+  }};
+  border-radius: 6px;
+  background-color: ${({variant, state, theme}) => {
+    if (state === "disabled") {
+      return theme.color.global.neutral[300];
+    } else {
+      if (variant === "default") {
+        return theme.color.sys.secondary.disabled;
+      } else {
+        return theme.color.sys.destructive.disabled;
+      }
+    }
+  }};
+  padding: ${({state}) => {
+    return state === "focused" ? "4px" : "0px";
+  }};
+`;
+
+const InputContainer = styled.View<InputProps & { theme: DefaultTheme }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+  flex-direction: row;
+  padding: 12px 14px;
+  border: ${({state, variant, theme}) => {
+    if (state === "focused") {
+      return `1px solid ${
+          variant === "default"
+              ? theme.color.sys.secondary.default
+              : theme.color.sys.destructive.default
+      }`;
+    } else {
+      return 'none';
+    }
+  }};
+  background-color: ${({variant, state, theme}) => {
+    return state === "disabled" ? theme.color.global.neutral[300] : theme.color.global.neutral[100];
+  }};
+  border-radius: 6px;
+`;
+
+const StyledInput = styled(TextInput).attrs(({theme}: { theme: DefaultTheme }) => ({
+  placeholderTextColor: theme.color.theme.disabled,
+}))`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16.94px;
+
+  color: ${({theme}) => theme.color.global.neutral[900]};
+`;
+
+const CloseButton = styled(TouchableOpacity)`
+`;
+
+const DescriptionText = styled(BodyRegular12)<InputProps & { theme: DefaultTheme }>`
+  font-style: normal;
+  font-weight: 400;
+
+  margin-left: 3px;
+  margin-top: 5px;
+  color: ${({variant, theme}) =>
+      variant === "destructive"
+          ? theme.color.sys.destructive.default
+          : theme.color.global.neutral[700]};
+`
+
+export default Input;
