6c96cda5e5b1f388145c3811c0d333e74efc099a
parksol <thfdl0317@naver.com>
2024-09-21 13:55:41 +0900

    fix : sm, md에 따라 tag크기 변경,  mode="info"일 경우 border색상 반영

--- src/common/Tag.tsx
+++ src/common/Tag.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import styled from "styled-components/native";
-import ThemeStyle from "../styles/ThemeStyle"; // ThemeStyle 가져오기
-
+import ThemeStyle from "../styles/ThemeStyle";
 interface TagProps {
+  label: string;
   height?: "sm" | "md";
-  theme?: (typeof ThemeStyle)["color"]; // theme 타입 지정
+  theme?: (typeof ThemeStyle)["color"];
   fontSize?: "sm" | "md";
   padding?: "sm" | "md";
   textColor?: string;
   bgColor?: string;
   borderColor?: string;
   outline?: boolean;
-  label: string;
   mode?: "info" | "success" | "error" | "neutral" | "primary";
 }
 
-const modeStyles = (theme: any) => ({
+const modeStyles = (theme: string) => ({
   info: {
     textColor: theme.color.global.blue[600],
     bgColor: theme.color.global.blue[100],
@@ -44,27 +43,26 @@ const modeStyles = (theme: any) => ({
 });
 
 const TagWrap = styled.Text<TagProps>`
-  padding: ${(props) => (props.padding === "sm" ? "3px 4px" : "2px 6px")};
+  padding: ${(props) => (props.height === "sm" ? "3px 4px" : "2px 6px")};
   height: ${(props) => (props.height === "sm" ? "20px" : "28px")};
+  font-size: ${(props) => (props.height === "sm" ? "11px" : "16px")};
   border-radius: 4px;
-  font-size: ${(props) => (props.fontSize === "sm" ? "11px" : "16px")};
   text-align: center;
 
   color: ${(props) => modeStyles(props.theme)[props.mode || "info"].textColor};
   background-color: ${(props) => modeStyles(props.theme)[props.mode || "info"].bgColor};
   border: ${(props) =>
-    props.outline ? `1px solid ${modeStyles(props.theme)[props.mode || "info"].borderColor}` : "none"};
+    props.outline || props.mode === "info"
+      ? `1px solid ${modeStyles(props.theme)[props.mode || "info"].borderColor}`
+      : "none"};
 `;
 
-const Tag = ({ label, ...props }: TagProps) => {
-  return <TagWrap {...props}>{label}</TagWrap>;
-};
-
-Tag.defaultProps = {
-  height: "md",
-  fontSize: "md",
-  padding: "md",
-  mode: "info",
+const Tag = ({ label, height = "md", fontSize = "md", padding = "md", mode = "info", ...props }: TagProps) => {
+  return (
+    <TagWrap height={height} fontSize={fontSize} padding={padding} mode={mode} {...props}>
+      {label}
+    </TagWrap>
+  );
 };
 
 export default Tag;
--- src/components/sample/SampleTag.tsx
+++ src/components/sample/SampleTag.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import styled from "styled-components/native";
 import Tag from "../../common/Tag";
 
 function SampleTag() {
   return (
     <ScrollViewContainer keyboardShouldPersistTaps="handled">
-      <Tag mode="info" label="Info" />
-      <Tag mode="success" label="Success" />
-      <Tag mode="error" label="Error" />
-      <Tag mode="neutral" label="Neutral" />
-      <Tag mode="primary" outline label="Primary" />
+      <TagGroup>
+        <Tag mode="info" label="Info" />
+        <Tag mode="success" label="Success" />
+        <Tag mode="error" label="Error" />
+        <Tag mode="neutral" label="Neutral" />
+        <Tag mode="primary" outline label="Primary" />
+      </TagGroup>
+
+      <TagGroup>
+        <Tag mode="info" label="Info" height="sm" />
+        <Tag mode="success" label="Success" height="sm" />
+        <Tag mode="error" label="Error" height="sm" />
+        <Tag mode="neutral" label="Neutral" height="sm" />
+        <Tag mode="primary" outline label="Primary" height="sm" />
+      </TagGroup>
     </ScrollViewContainer>
   );
 }
@@ -19,13 +29,19 @@ const ScrollViewContainer = styled(ScrollView).attrs({
   contentContainerStyle: {
     justifyContent: "center",
     alignItems: "center",
-    padding: 16,
-    gap: 12,
+    paddingVertical: 16,
   },
 })`
   padding: 20px;
   flex: 1;
   background-color: #ffffff;
+`;
+
+const TagGroup = styled(View)`
+  padding: 12px;
+  justify-content: space-between;
+  align-items: center;
+  gap: 12px;
 `;
 
 export default SampleTag;
