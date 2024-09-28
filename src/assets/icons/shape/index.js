d3e11870b759f8219a73570f5b0e61830890f004
Bob Sin <kd.sin@bucketplace.net>
2024-09-28 17:54:08 +0900

    [KAN-215] toast 작업 - svg 파일 추가

--- src/assets/icons/shape/index.js
+++ src/assets/icons/shape/index.js
@@ -1,3 +1,11 @@
 import CircleClose from "./circle-close.svg";
+import CheckCircleGreen from "./check-circle-green.svg";
+import Warning from "./warning.svg";
+import InformationCircle from "./information-circle.svg";
 
-export { CircleClose };
+export {
+    CircleClose,
+    CheckCircleGreen,
+    Warning,
+    InformationCircle,
+};
