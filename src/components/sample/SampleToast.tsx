4112e3bbf9f660e3a85978463461e289113fe778
sinkyoungdeok <tkagmd1@naver.com>
2024-10-06 13:59:59 +0900

    [KAN-215] 사이공간 추가

--- src/components/sample/SampleToast.tsx
+++ src/components/sample/SampleToast.tsx
@@ -6,11 +6,21 @@ import Toast from "../../common/atom/Toast";
 const SampleToast = () => {
   return (
     <ScrollViewContainer keyboardShouldPersistTaps="handled">
-      <Toast message="댓글 알림이 활성화 되었습니다." variant="default" />
-      <Toast message="댓글 알림이 활성화 되었습니다." variant="success" />
-      <Toast message="댓글 알림이 활성화 되었습니다." variant="destructive" />
-      <Toast message="댓글 알림이 활성화 되었습니다." variant="informative" />
-      <Toast message="댓글 알림이 활성화 되었습니다." variant="default" isNavigateButton={false} />
+      <ToastWrapper>
+        <Toast message="댓글 알림이 활성화 되었습니다." variant="default" />
+      </ToastWrapper>
+      <ToastWrapper>
+        <Toast message="댓글 알림이 활성화 되었습니다." variant="success" />
+      </ToastWrapper>
+      <ToastWrapper>
+        <Toast message="댓글 알림이 활성화 되었습니다." variant="destructive" />
+      </ToastWrapper>
+      <ToastWrapper>
+        <Toast message="댓글 알림이 활성화 되었습니다." variant="informative" />
+      </ToastWrapper>
+      <ToastWrapper>
+        <Toast message="댓글 알림이 활성화 되었습니다." variant="default" isNavigateButton={false} />
+      </ToastWrapper>
     </ScrollViewContainer>
   );
 };
@@ -18,6 +28,10 @@ const SampleToast = () => {
 const ScrollViewContainer = styled(ScrollView)`
   padding: 20px;
   background-color: #ffffff;
+`;
+
+const ToastWrapper = styled.View`
+  margin-bottom: 10px;
 `;
 
 export default SampleToast;
