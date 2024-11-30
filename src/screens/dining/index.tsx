78cd4064d0ab0e5205abfb23c21f3b4397cc62f7
parksol <thfdl0317@naver.com>
2024-11-30 18:00:21 +0900

    add : Dining관련 폴더, 파일, 컴포넌트 생성 및 분리, 코드 적용, 음식 카테고리 아이콘 추가

---
+++ src/screens/dining/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { StyleSheet, ScrollView } from "react-native";
+
+import Toast from "../../common/atom/Toast";
+import DiningHeader from "./components/templates/DiningHeader";
+import DiningMain from "./components/templates/DiningMain";
+
+interface ToastHandle {
+  showToast: () => void;
+}
+export const DiningPage = () => {
+  const toastRef = React.createRef<ToastHandle>();
+  const [toastVariant, setToastVariant] = useState<string | null>(null);
+
+  return (
+    // DOTO : 스크롤 공통 스타일 적용
+    <ScrollView contentContainerStyle={styles.container}>
+      <DiningHeader />
+      <DiningMain />
+
+      <Toast ref={toastRef} message="안녕하세요, {닉네임}님!" variant={toastVariant} />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
+    padding: 18,
+  },
+});
+
+export default DiningPage;
