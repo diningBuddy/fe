8636efbb0a42990b086f716558b016655aa72288
daeseopk <eotjqdlee@gmail.com>
2025-03-21 10:48:04 +0900

    [KAN-327] adjust eslint rules

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const { getDefaultConfig } = require("expo/metro-config");
+import { getDefaultConfig } from "@expo/config";
 
 module.exports = (() => {
   const config = getDefaultConfig(__dirname);
